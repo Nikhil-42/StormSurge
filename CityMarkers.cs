68736ad760007bbad7b2207262f1ad8846fad41a
Catherine Wu <139652546+catherineewu@users.noreply.github.com>
2025-06-09 14:16:23 +0900

    script creates map marker pins at different positions

---
+++ CityMarkers.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class CityMarkers : Sprite3D
+{
+	public override void _Ready() {
+		GD.Print("Running map marker script.");
+		
+	}
+	
+	public override void _Process(double delta) {
+		
+	}
+}
