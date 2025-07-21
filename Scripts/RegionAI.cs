cec22fbdbcdbd153272b5cd0ad39a72fc5889d1d
Catherine Wu <139652546+catherineewu@users.noreply.github.com>
2025-07-21 21:52:42 +0900

    determine characteristics from statistics
    small changes

--- Scripts/RegionAI.cs
+++ Scripts/RegionAI.cs
@@ -119,6 +119,9 @@ public partial class RegionAI
 	public RegionStats _regionStats;
 
 	public class Characteristics {  // Variables different for each country that are calculated based on stats
+		// FIXME: Characteristics should be based on more detailed statistics for countries (storm susceptibility, etc.), but
+		// for now everything based on GDP or development pretty much
+
 		// MONIE MATTERS
 		public double income;  // Region usable income based on GDP and population
 		public double globalResearchFunding;  // % usual contribution of income to global research
@@ -128,12 +131,12 @@ public partial class RegionAI
 		public double midHealth;  // Switch to recovery/research
 		public double poorHealth;  // Switch to recovery
 
-		public double goodMoney;  // Switch from savings to debauchery, full research investment (rarely happens for poorest regions)
-		public double midMoney;  // Savings, partial research investment
-		public double poorMoney;  // Savings, no research investment
+		public double goodMoney;  // (% of GDP) Switch from savings to debauchery, full research investment (rarely happens for poorest regions)
+		public double midMoney;  // (% of GDP) Savings, partial research investment
+		public double poorMoney;  // (% of GDP) Savings, no research investment
 
-		public double lowAlarmThreshold;  // No debauchery, fund research (75% income), upgrades (25% income) instead
-		public double highAlarmThreshold;  // Upgrades (75% income), and research (25% income)
+		public double lowAlarmThreshold;  // (% of Total Damage) No debauchery, fund research (75% income), upgrades (25% income) instead
+		public double highAlarmThreshold;  // (% of Total Damage) Upgrades (75% income), and research (25% income)
 
 		// DAMAGE RATE
 		public double windDamageMultiplier;  // Base 1.0, affected by infrastructure and preparedness
@@ -149,21 +152,57 @@ public partial class RegionAI
 		public double stormPreparedness;  // Base 1.0 multiplier for countries that regularly experience storms normally (based on GDP + coastal pop for now)
 
 		public Characteristics() {
+			// income, globalResearchFunding, Money's, emissions
 			double PerCapitaGDP = (_regionStats.GDP * 1000) / _regionStats.Population;
 			if (PerCapitaGDP > 50000) {
 				income = _regionStats.GDP * 0.6;
 				globalResearchFunding = 0.5;
+
+				goodMoney = 0.9;
+				midMoney = 0.75;
+				poorMoney = 0.5;
 			} else if (PerCapitaGDP > 20000) {
 				income = _regionStats.GDP * 0.4;
 				globalResearchFunding = 0.2;
+
+				goodMoney = 0.8;
+				midMoney = 0.6;
+				poorMoney = 0.3;
 			} else if (PerCapitaGDP > 10000) {
 				income = _regionStats.GDP * 0.3;
 				globalResearchFunding = 0.1;
+
+				goodMoney = 0.7;
+				midMoney = 0.5;
+				poorMoney = 0.2;
 			} else {
 				income = _regionStats.GDP * 0.2;
 				globalResearchFunding = 0.05;
+
+				goodMoney = 0.6;
+				midMoney = 0.4;
+				poorMoney = 0.2;
 			}
-			income = _regionStats.GDP;
+			emissions = _regionStats.GDP / 1000;
+
+			// Health's, alarm thresholds, damage multipliers, etc.
+			goodHealth = 0.8 + (0.2 * _regionStats.DevelopmentIndex);
+			midHealth = 0.6 + (0.4 * _regionStats.DevelopmentIndex);
+			poorHealth = 0.4 + (0.6 * _regionStats.DevelopmentIndex);
+
+			lowAlarmThreshold = 0.9 + (0.1 * _regionStats.DevelopmentIndex);
+			highAlarmThreshold = 0.75 + (0.25 * _regionStats.DevelopmentIndex);
+
+			windDamageMultiplier = 1 + ((1 - _regionStats.DevelopmentIndex)/2);
+			floodDamageMultiplier = 1 + _regionStats.CoastalPopulation;
+			secondaryDamageMultiplier = 1 + (1 - _regionStats.DevelopmentIndex);
+
+			governmentEfficiency = 0.5 + _regionStats.DevelopmentIndex;
+			internationalRelations = 0.5 + _regionStats.DevelopmentIndex;
+			education = 0.5 + _regionStats.DevelopmentIndex;
+			buildingInfrastructure = 0.5 + _regionStats.DevelopmentIndex;
+
+			stormPreparedness = 0.5 + (_regionStats.DevelopmentIndex * _regionStats.CoastalPopulation);
 		}
 	} 
 
